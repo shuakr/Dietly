db284022b9c558b2aeb3c0b66ef042e77a10249c
4liriza <malirizabagci1@gmail.com>
2025-05-02 21:32:38 +0300

    Add: profile_creation_screen.dart and profile creation screen

---
+++ lib/screens/profile_creation_screen.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // Font Awesome ikonları için
+
+class ProfileCreationScreen extends StatefulWidget {
+  const ProfileCreationScreen({super.key});
+
+  @override
+  State<ProfileCreationScreen> createState() => _ProfileCreationScreenState();
+}
+
+class _ProfileCreationScreenState extends State<ProfileCreationScreen> {
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _birthDateController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+  String? _selectedGender;
+  IconData? _selectedProfileIcon;
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        _birthDateController.text = DateFormat('dd/MM/yyyy').format(picked);
+      });
+    }
+  }
+
+  void _showIconSelectionBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 200,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+            ),
+            itemCount: _profileIcons.length,
+            itemBuilder: (context, index) {
+              final iconData = _profileIcons[index];
+              return IconButton(
+                icon: Icon(iconData, size: 30),
+                onPressed: () {
+                  setState(() {
+                    _selectedProfileIcon = iconData;
+                  });
+                  Navigator.pop(context);
+                },
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  final List<IconData> _profileIcons = [
+    FontAwesomeIcons.user,
+    FontAwesomeIcons.heartbeat,
+    FontAwesomeIcons.rulerVertical,
+    FontAwesomeIcons.weightScale,
+    FontAwesomeIcons.mars,
+    FontAwesomeIcons.venus,
+    FontAwesomeIcons.utensils,
+    FontAwesomeIcons.walking,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF1E3D3),
+      resizeToAvoidBottomInset: true, // Klavye ile taşmayı önlemek için
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1B4D3E),
+        title: const Text('Create a Profile'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            GestureDetector(
+              onTap: () => _showIconSelectionBottomSheet(context),
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: Icon(
+                  _selectedProfileIcon ?? FontAwesomeIcons.user,
+                  size: 60,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            TextFormField(
+              controller: _fullNameController,
+              decoration: InputDecoration( // const kaldırıldı
+                labelText: 'Full Name',
+                prefixIcon: const Icon(FontAwesomeIcons.user),
+                hintText: 'Your full name',
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _birthDateController,
+              decoration: InputDecoration( // const kaldırıldı
+                labelText: 'Date of Birth (DD/MM/YYYY)',
+                prefixIcon: const Icon(FontAwesomeIcons.calendar),
+                hintText: 'DD/MM/YYYY',
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.date_range),
+                  onPressed: () => _selectDate(context),
+                ),
+              ),
+              readOnly: true,
+              onTap: () => _selectDate(context),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _heightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration( // const kaldırıldı
+                labelText: 'Height (cm)',
+                prefixIcon: const Icon(FontAwesomeIcons.rulerVertical),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                border:  OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _weightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration( // const kaldırıldı
+                labelText: 'Weight (kg)',
+                prefixIcon: const Icon(FontAwesomeIcons.weightScale),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            InputDecorator(
+              decoration: InputDecoration( // const kaldırıldı
+                labelText: 'Gender',
+                prefixIcon: const Icon(FontAwesomeIcons.venusMars),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                border:  OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RadioListTile<String>(
+                      title: const Text('Male'),
+                      value: 'male',
+                      groupValue: _selectedGender,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedGender = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile<String>(
+                      title: const Text('Female'),
+                      value: 'female',
+                      groupValue: _selectedGender,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedGender = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 32.0),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Profil oluşturma işlemleri
+                      print('Profile created(for myself).');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF1B4D3E),
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Create(for myself)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Profil oluşturma işlemleri (başkası için)
+                      print('Profile created(for other person).');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF1B4D3E),
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Create(for other person)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
