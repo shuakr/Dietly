49bb00fcd43f570f01e0c2f10393bbd367d49ea6
4liriza <malirizabagci1@gmail.com>
2025-05-02 21:37:53 +0300

    feat: changed routes and added main theme for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart'; // Ekranımızı import ediyoruz
+import 'screens/login_screen.dart'; // Giriş ekranını import ediyoruz
+import 'screens/register_screen.dart'; // Kayıt ekranını import ediyoruz
+import 'screens/profile_creation_screen.dart'; // Profil oluşturma ekranını import ediyoruz
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(), // İşte burada LoginScreen'i açıyoruz
+      home: const LoginPage(), // When the app starts we see login page
+      routes: {
+        '/register': (context) => const RegisterPage(), // Route for register page
+        '/profileCreation': (context) => const ProfileCreationScreen(), // Route for profile creation page
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.green, // Main theme color
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF1B4D3E), // Main AppBar color
+        ),
+      ),
     );
   }
 }
