b0c1f996995fcca1cad92dbab31eb9c9fc30da89
YUSUFTALHA <434385@ogr.ktu.edu.tr>
2025-05-15 00:51:47 +0300

    feat(app): Refactor app navigation by setting SplashScreen as home and defining routes, and implement base theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:dietly/screens/profile_creation_screen.dart';
+import 'package:dietly/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart'; // Ekranımızı import ediyoruz
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:dietly/screens/splash_screen.dart';
 
 
 void main() async {
@@ -19,7 +22,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(), // İşte burada LoginScreen'i açıyoruz
+      home: const SplashScreen(), // Splash artık ilk açılıyor
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/profileCreation': (context) => const ProfileCreationScreen(),
+      },
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF800020),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFF5E6D6),
+        ),
+      ),
     );
   }
 }
