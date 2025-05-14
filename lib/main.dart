0242abc14f76b53143a6cbf285da07c87bb1068f
YUSUFTALHA <434385@ogr.ktu.edu.tr>
2025-05-14 22:36:14 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'screens/login_screen.dart'; // Giriş ekranını import ediyoruz
-import 'screens/register_screen.dart'; // Kayıt ekranını import ediyoruz
-import 'screens/profile_creation_screen.dart'; // Profil oluşturma ekranını import ediyoruz
-import 'screens/splash_screen.dart'; // Yükleme ekran animasyon dosyasını import ediyoruz.
-=======
 import 'screens/login_screen.dart'; // Ekranımızı import ediyoruz
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
->>>>>>> develop
 
 
 void main() async {
@@ -25,21 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dietly',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => const LoginPage(),
-        '/register': (context) => const RegisterPage(), // Route for register page
-        '/profileCreation': (context) => const ProfileCreationScreen(), // Route for profile creation page
-      },
-      theme: ThemeData(
-        primarySwatch: Colors.green, // Main theme color
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF1B4D3E), // Main AppBar color
-        ),
-      ),
+      home: const LoginPage(), // İşte burada LoginScreen'i açıyoruz
     );
   }
 }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -25,161 +25,55 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFFF5E6D6),
-        resizeToAvoidBottomInset: false,
-        body: Stack(
-            children: <Widget> [
-              const Align(
-                alignment: Alignment.bottomCenter,
-                child: BottomShape(),
+      backgroundColor: const Color(0xFFF5E6D6),
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: <Widget> [
+      const Align(
+      alignment: Alignment.bottomCenter,
+        child: BottomShape(),
+      ),
+      SingleChildScrollView(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 48),
+            Center(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                  ),
+                  Transform.scale(
+                      scale: 4.250,
+                      child: Image.asset(
+                        'assets/dietlylogo.png',
+                        height: 100,
+                        width: 100,
+                      )
+                  )
+                ],
               ),
-              SingleChildScrollView(
-                padding: const EdgeInsets.all(32),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const SizedBox(height: 48),
-                    Center(
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          SizedBox(
-                            width: 120,
-                            height: 120,
-                          ),
-                          Transform.scale(
-                              scale: 4.250,
-                              child: Image.asset(
-                                'assets/dietlylogo.png',
-                                height: 100,
-                                width: 100,
-                              )
-                          )
-                        ],
-                      ),
-                    ),
+            ),
 
-                    const SizedBox(height: 48),
+            const SizedBox(height: 48),
 
-                    const Text(
-                        'Full Name', style: TextStyle(fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 8),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your full name',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    const Text('Email', style: TextStyle(fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 8),
-                    TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        hintText: 'your@example.com',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    const Text(
-                        'Password', style: TextStyle(fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 8),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: '********',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    const Text('Confirm Password',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 8),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Re-enter your password',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 20),
-
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/profileCreation');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF800020),
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      child: const Text(
-                        'REGISTER',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 10),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text("Allready have an account?"),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LoginPage()),
-                            );
-                          },
-                          child: const Text(
-                            "Log In!",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+            const Text(
+                'Full Name', style: TextStyle(fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Your full name',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
                 ),
               ),
-<<<<<<< HEAD
-            ]
-        )
-=======
             ),
 
             const SizedBox(height: 16),
@@ -310,7 +204,6 @@ class _RegisterPageState extends State<RegisterPage> {
       ),
         ]
       )
->>>>>>> develop
     );
   }
 }
