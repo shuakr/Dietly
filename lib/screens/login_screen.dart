39a8811b59ea4c9020bfd01a989c2716525867b2
YUSUFTALHA <434385@ogr.ktu.edu.tr>
2025-05-18 01:17:13 +0300

    fix: Change some syntax for an error message.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -218,7 +218,7 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 16),
 
             ElevatedButton.icon(
-              onPressed: () => _handleGoogleSignIn(context),
+              onPressed: _handleGoogleSignIn,
               icon: const Icon(Bootstrap.google, color: Colors.white),
               label: const Text(
                 'Google',
