da884183f1467f7397f2e814f8bba15907da09c8
YUSUFTALHA <434385@ogr.ktu.edu.tr>
2025-05-18 01:14:51 +0300

    feat(auth): Add firebase_auth.dart to login_screen for sign_in check.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dietly/screens/profile_creation_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:dietly/widgets/bottom_shape.dart';
