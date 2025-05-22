62ce4cc76fb523c5555321e117ceef79a936a2d4
CemOnat <satircemonat@gmail.com>
2025-05-23 02:54:35 +0300

    Setup basic Scaffold and define main colors

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const mainColor = Color(0xFF6B0010);
+    const bgColor = Color(0xFFF5E7D0);
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Bu kısım sonraki commitlerde eklenecek
+          ],
+        ),
+      ),
+    );
+  }
+}
