b0eab5cf50c8ffce554229774682e12b1c4f7dfe
YUSUFTALHA <434385@ogr.ktu.edu.tr>
2025-05-24 01:32:57 +0300

    refactor: HomeScreen to use StatelessWidget and delegate navigation to CustomBottomNavBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,25 +1,10 @@
-import 'package:flutter/foundation.dart';
+// lib/screens/home_screen.dart
+
 import 'package:flutter/material.dart';
-import 'package:dietly/widgets/salomon_bottom_bar.dart'; //Import bottom shape
-import 'package:firebase_auth/firebase_auth.dart';
+import '../widgets/salomon_bottom_bar.dart';
 
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-
-  final List<Widget> _pages = [
-    const Center(child: Text('Ana Sayfa')),
-    const Center(child: Text('Bildirimler')),
-    const Center(child: Text('Profil')),
-    const Center(child: Text('Çıkış')),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,34 +13,8 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Dietly Home'),
         centerTitle: true,
       ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: CustomBottomNavBar(
-        currentIndex: _currentIndex,
-        onTap: (index) async {
-          if (index == 3) {
-            await FirebaseAuth.instance.signOut();
-
-            if (context.mounted) {
-              Navigator.pushReplacementNamed(context, '/login');
-            }
-
-          } else {
-            setState(() {
-              _currentIndex = index;
-            });
-          }
-          if (FirebaseAuth.instance.currentUser == null) {
-            if (kDebugMode) {
-              print('✅ Çıkış başarılı: currentUser == null');
-            }
-          } else {
-            if (kDebugMode) {
-              print('❌ Çıkış başarısız: currentUser != null');
-            }
-          }
-
-        },
-      ),
+      body: const Center(child: Text('Ana Sayfa')),
+      bottomNavigationBar: CustomBottomNavBar(initialIndex: 0),
     );
   }
 }
